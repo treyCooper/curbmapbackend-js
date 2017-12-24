69f3f19351aac1b34ac1ad05b1afbc2ebf76bc02
Eli Selkin <ejselkin@cpp.edu>
2017-12-23 16:59:38 -0800

    more lines lines lines

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const cors = require("cors");
-// require("dotenv").config({ path: "../curbmap.env" });
-require("dotenv").config({ path: "../curbmap2.env" });
+require("dotenv").config({path: "../curbmap.env"});
 const postgres = require("./model/postgresModels");
 const RedisStore = require("connect-redis")(session);
 const redis = require("redis").createClient(50005, "127.0.0.1");
@@ -27,15 +26,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // CORS setup
-var whitelist = [
-    "http://localhost:8080",
-    "https://curbmap.com",
-    "https://curbmap.com:443",
-    "http://curbmap.com:8080",
-    "*"
-];
+var whitelist = ["http://localhost:8080", "https://curbmap.com", "https://curbmap.com:443", "http://curbmap.com:8080", "*"];
 var corsOptions = {
-    origin: function(origin, callback) {
+    origin: function (origin, callback) {
         winston.log("info", "origin: ", origin);
         if (whitelist.indexOf("*") !== -1 || whitelist.indexOf(origin) !== -1) {
             callback(null, true);
@@ -52,7 +45,7 @@ app.use(logger("dev"));
 app.use(cookieParser());
 // routes can also get the body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
 function shouldCompress(req, res) {
@@ -66,48 +59,47 @@ function shouldCompress(req, res) {
 }
 
 // Session stuff
-app.use(
-    session({
-        store: new RedisStore({
-            host: "127.0.0.1",
-            port: 50005,
-            prefix: "curbmap:sessions:",
-            client: redis,
-            ttl: 100
-        }),
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.REDIS_SECRET
-    })
-);
+app.use(session({
+    store: new RedisStore({host: "127.0.0.1", port: 50005, prefix: "curbmap:sessions:", client: redis, ttl: 100}),
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.REDIS_SECRET
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
     cb(null, user.username);
 });
 
-passport.deserializeUser(function(username, cb) {
+passport.deserializeUser(function (username, cb) {
     findUser(username, cb);
 });
 
 function findUser(username, cb) {
-    postgres.User.findOne({ where: { username: username } }).then(function(foundUser) {
-        if (foundUser !== null) {
-            return cb(null, foundUser);
-        } else {
-            return cb(null, false);
-        }
-    });
+    postgres
+        .User
+        .findOne({
+            where: {
+                username: username
+            }
+        })
+        .then(function (foundUser) {
+            if (foundUser !== null) {
+                return cb(null, foundUser);
+            } else {
+                return cb(null, false);
+            }
+        });
 }
 passport.authMiddleware = require("./auth/authMiddleware");
 
 // We will add other Strategies, such as FB strategy
-passport.use(
-    new LocalStrategy(function(username, password, done) {
-        findUser(username, function(nullvalue, userObject) {
-            if (userObject !== false) {
-                bcrypt.compare(password, userObject.password_hash, function(err, res) {
+passport.use(new LocalStrategy(function (username, password, done) {
+    findUser(username, function (nullvalue, userObject) {
+        if (userObject !== false) {
+            bcrypt
+                .compare(password, userObject.password_hash, function (err, res) {
                     if (err) {
                         return done(err);
                     } else if (res === true) {
@@ -116,26 +108,29 @@ passport.use(
                         return done(null, false);
                     }
                 });
-            } else {
-                return done(null, false);
-            }
-        });
-    })
-);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
 require("./routes/index").init(app, redis);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.locals.error = req
+        .app
+        .get("env") === "development"
+        ? err
+        : {};
 
     // render the error page
     res.status(err.status || 500);
--- model/mongooseModels.js
+++ model/mongooseModels.js
@@ -1,111 +1,124 @@
 "use strict";
 const mongoose = require("mongoose");
 const GeoJSON = require("mongoose-geojson-schema");
-/*const uri = `mongodb://${process.env.MONGO_SHARD_0},${process.env.MONGO_SHARD_1},${process.env.MONGO_SHARD_2}/${
-    process.env.MONGO_DB
-}?ssl=true&replicaSet=${process.env.MONGO_REPLSET}`;*/
-const uri = "mongodb://" + process.env.MONGO_ADDRESS + "/" + process.env.MONGO_DB;
-console.log(uri);
-mongoose.connect(uri, {
-    user: process.env.MAPDB_USERNAME,
-    pass: process.env.MAPDB_PASSWORD,
-    useMongoClient: true
-});
+const uri = `mongodb://${process.env.MONGO_SHARD_0},${process.env.MONGO_SHARD_1},${process.env.MONGO_SHARD_2}/${process.env.MONGO_DB}?ssl=true&replicaSet=${process.env.MONGO_REPLSET}&authSource=admin`;
+
 // mongoose.connect(uri);
 mongoose.Promise = require("bluebird");
 
-const HeatMapSchema = new mongoose.Schema(
-    {
-        six_sig_olc: String, // Must actually be full 9 characters with two 00 padding and +
-        corners: [[{ type: Number }]], // Two opposite corners of region (most likely NW, SE)
-        total_types: { type: Number, default: 0 },
-        types_each: [Number] // Processed in batch on the backend, 14 or however types we end up having
-    },
-    {
-        collection: "HeatMaps"
-    }
-);
-
-const RestrSchema = new mongoose.Schema(
-    {
-        tp: Number, // type {0-13}
-        an: Number, // angle
-        st: Number, // start
-        ed: Number, // end
-        ds: [Boolean], // days in effect
-        wk: [Boolean], // weeks in effect
-        mn: [Boolean], // months in effect
-        lt: Number, // limit
-        pm: String, // permit
-        ct: Number, // cost
-        pr: Number, // per
-        mo: Boolean, // motorcycle parking space
-        up: Number, // upVotes
-        dn: Number, // downVotes
-        by: String, // by user id
-        ud: mongoose.SchemaTypes.Date // updatedOn
-    },
-    {
-        usePushEach: true
-    }
-);
-
-const MapLineSchema = new mongoose.Schema(
-    {
-        loc: { type: mongoose.Schema.Types.LineString, index: "2dsphere" },
-        restrs_length: { type: Number, default: 0 },
-        restrs: { type: [RestrSchema], default: [] }
-    },
-    {
-        usePushEach: true
-    }
-);
-
-const MapLineWithoutParentsSchema = new mongoose.Schema(
-    {
-        loc: { type: mongoose.Schema.Types.LineString, index: true },
-        restrs_length: { type: Number, default: 0 },
-        restrs: { type: [RestrSchema], default: [] }
-    },
-    {
-        collection: "LinesWithoutParents",
-        autoIndexId: true,
-        usePushEach: true
-    }
-);
-
-const MapLineParentSchema = new mongoose.Schema(
-    {
-        local_map_code: String, // i.e. CAMS id
-        loc: { type: mongoose.Schema.Types.LineString, index: "2dsphere" },
-        fullname: String,
-        status: String,
+const HeatMapSchema = new mongoose.Schema({
+    six_sig_olc: {
         type: String,
-        city: String,
-        from: Number,
-        to: Number,
-        zip: Number,
-        total_types: { type: Number, default: 0 },
-        types_each: [Number], // Processed in batch on the backend, 14 or however types we end up having
-        lines_length: { type: Number, default: 0 },
-        lines: { type: [MapLineSchema], default: [] }
+        index: true
+    }, // Must actually be full 9 characters with two 00 padding and +
+    corners: [
+        [
+            {
+                type: Number
+            }
+        ]
+    ], // Two opposite corners of region (most likely NW, SE)
+    total_types: {
+        type: Number,
+        default: 0
     },
-    {
-        collection: "MapLines",
-        usePushEach: true
+    types_each: [Number] // Processed in batch on the backend, 14 or however types we end up having
+}, {collection: "HeatMaps"});
+
+const RestrSchema = new mongoose.Schema({
+    tp: Number, // type {0-13}
+    an: Number, // angle
+    st: Number, // start
+    ed: Number, // end
+    ds: [Boolean], // days in effect
+    wk: [Boolean], // weeks in effect
+    mn: [Boolean], // months in effect
+    lt: Number, // limit
+    pm: String, // permit
+    ct: Number, // cost
+    pr: Number, // per
+    mo: Boolean, // motorcycle parking space
+    up: Number, // upVotes
+    dn: Number, // downVotes
+    by: String, // by user id
+    ud: mongoose.SchemaTypes.Date // updatedOn
+}, {usePushEach: true});
+
+const MapLineSchema = new mongoose.Schema({
+    loc: {
+        type: mongoose.Schema.Types.LineString,
+        index: "2dsphere"
+    },
+    restrs_length: {
+        type: Number,
+        default: 0
+    },
+    restrs: {
+        type: [RestrSchema],
+        default: []
     }
-);
+}, {usePushEach: true});
+
+const MapLineWithoutParentsSchema = new mongoose.Schema({
+    loc: {
+        type: mongoose.Schema.Types.LineString,
+        index: "2dsphere"
+    },
+    restrs_length: {
+        type: Number,
+        default: 0
+    },
+    restrs: {
+        type: [RestrSchema],
+        default: []
+    }
+}, {
+    collection: "Lines",
+    usePushEach: true
+});
+
+const MapLineParentSchema = new mongoose.Schema({
+    local_map_code: String, // i.e. CAMS id
+    loc: {
+        type: mongoose.Schema.Types.LineString,
+        index: "2dsphere"
+    },
+    fullname: String,
+    status: String,
+    type: String,
+    city: String,
+    from: Number,
+    to: Number,
+    zip: Number,
+    total_types: {
+        type: Number,
+        default: 0
+    },
+    types_each: [Number], // Processed in batch on the backend, 14 or however types we end up having
+    lines_length: {
+        type: Number,
+        default: 0
+    },
+    lines: {
+        type: [MapLineSchema],
+        default: []
+    }
+}, {
+    collection: "MapLines",
+    usePushEach: true
+});
 
 const MapLineParents = mongoose.model("MapLines", MapLineParentSchema);
-const MapLinesWithoutParents = mongoose.model("LinesWithoutParents", MapLineWithoutParentsSchema);
+const MapLinesWithoutParents = mongoose.model("Lines", MapLineWithoutParentsSchema);
 const HeatMaps = mongoose.model("HeatMaps", HeatMapSchema);
-const MapLines = mongoose.model("Lines", MapLineSchema);
-const Restrs = mongoose.model("Restrs", RestrSchema);
+mongoose.connect(uri, {
+    user: process.env.MAPDB_USERNAME,
+    pass: process.env.MAPDB_PASSWORD,
 
+    useMongoClient: true
+});
 module.exports = {
     parents: MapLineParents,
-    lines: MapLines,
     linesWithoutParents: MapLinesWithoutParents,
-    restrs: Restrs,
     obj_id: mongoose.Types.ObjectId
 };
