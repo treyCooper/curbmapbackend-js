fc4597c106a8da12e468601c19d58e7f0aa55644
Eli Selkin <ejselkin@cpp.edu>
2017-12-25 14:33:17 -0800

    check OLC after move

--- routes/main.js
+++ routes/main.js
@@ -373,7 +373,7 @@ function api(app, redisclient) {
         if (findExists(req.session.role, levels.user)) {
             try {
                 fs.renameSync(req.file.path, req.file.path + "-" + req.body.olc + ".jpg");
-                if (req.file.size < 10000) {
+                if (req.file.size < 10000 || req.body.olc === undefined || req.body.olc === "") {
                     fs.unlinkSync(req.file.path + "-" + req.body.olc + ".jpg");
                     res.status(400).json({
                         success: false,
