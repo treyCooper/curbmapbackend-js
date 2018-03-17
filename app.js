6d78faf271711b75025dbeabf1af50d81e6d3bde
Eli Selkin <ejselkin@cpp.edu>
2018-03-17 00:33:48 -0700

    refactor and update with 1.0 release of swagger API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
+require("dotenv").config({ path: "../curbmap.env" });
 const express = require("express");
-const session = require("express-session");
 const path = require("path");
 const logger = require("morgan");
 const passport = require("passport");
@@ -9,16 +9,13 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const cors = require("cors");
-require("dotenv").config({ path: "../curbmap.env" });
-const postgres = require("./model/postgresModels");
-const RedisStore = require("connect-redis")(session);
-const redis = require("redis").createClient(50005, "127.0.0.1");
 const compression = require("compression");
 const winston = require("winston");
 const bcrypt = require("bcrypt");
-
-redis.auth(process.env.REDIS_PASSWORD);
-
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const TOKEN_KEY = fs.readFileSync("../curbmap.pub");
 var app = express();
 
 // view engine setup
@@ -65,23 +62,7 @@ function shouldCompress(req, res) {
   return compression.filter(req, res);
 }
 
-// Session stuff
-app.use(
-  session({
-    store: new RedisStore({
-      host: "127.0.0.1",
-      port: 50005,
-      prefix: "curbmap:sessions:",
-      client: redis,
-      ttl: 100
-    }),
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.REDIS_SECRET
-  })
-);
 app.use(passport.initialize());
-app.use(passport.session());
 
 passport.serializeUser(function(user, cb) {
   cb(null, user.username);
@@ -91,42 +72,21 @@ passport.deserializeUser(function(username, cb) {
   findUser(username, cb);
 });
 
-function findUser(username, cb) {
-  postgres.User.findOne({
-    where: {
-      username: username
-    }
-  }).then(function(foundUser) {
-    if (foundUser !== null) {
-      return cb(null, foundUser);
-    } else {
-      return cb(null, false);
-    }
-  });
-}
-passport.authMiddleware = require("./auth/authMiddleware");
-
 // We will add other Strategies, such as FB strategy
 passport.use(
-  new LocalStrategy(function(username, password, done) {
-    findUser(username, function(nullvalue, userObject) {
-      if (userObject !== false) {
-        bcrypt.compare(password, userObject.password_hash, function(err, res) {
-          if (err) {
-            return done(err);
-          } else if (res === true) {
-            return done(null, userObject);
-          } else {
-            return done(null, false);
-          }
-        });
-      } else {
-        return done(null, false);
-      }
-    });
-  })
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: TOKEN_KEY,
+      algorithms: ["RS384"]
+    },
+    (jwtPayload, cb) => {
+      return cb(null, jwtPayload);
+    }
+  )
 );
-require("./routes/index").init(app, redis);
+const main = require("./routes/main")
+app.use("/", main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
